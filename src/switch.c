57175234c95f1a4f0ae7208380e2cc63cab35b43
pumpkin-cat <63824772+pumpkin-cat@users.noreply.github.com>
2023-01-07 15:01:37 +0800

    1.7 端侧协议+集成控制

--- src/switch.c
+++ src/switch.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -106,6 +107,7 @@ void init_port(){
 #endif
 	for(int i = 0; i < port_num; ++i){
 		sprintf(dev_group[i], "switch-iface%d", i+1);
+		open_pcap(dev_group[i], &pcap_handle_group[i]);
 	}
 }
 
@@ -114,189 +116,164 @@ void init_buffer(){
 #if TEST
 	printf("init_buffer\n");
 #endif
-	empty_num = SLOT_NUM;
-
-	used_slot_head = malloc(sizeof(ip_pcb_t));
-	unused_slot_head = malloc(sizeof(ip_pcb_t));
-	if(used_slot_head == NULL || unused_slot_head == NULL){
-		perror("init_buffer: ");
-		clean_exit();
-	}
-
-	memset(used_slot_head, 0, sizeof(ip_pcb_t));
-	memset(unused_slot_head, 0, sizeof(ip_pcb_t));
+	switch(rank){
+		case(SWITCH):
+			empty_num = SLOT_NUM;
+			used_slot_head = malloc(sizeof(ip_pcb_t));
+			unused_slot_head = malloc(sizeof(ip_pcb_t));
+			if(used_slot_head == NULL || unused_slot_head == NULL){
+				perror("init_buffer: ");
+				clean_exit();
+			}
+			last_used_slot = used_slot_head;
+			memset(used_slot_head, 0, sizeof(ip_pcb_t));
+			memset(unused_slot_head, 0, sizeof(ip_pcb_t));
 
 #if TEST
 	printf("malloc successful\n");
 #endif
-	
-	ip_pcb_t * prev, * current;
-	prev = unused_slot_head;
+		
+			ip_pcb_t * prev, * current;
+			prev = unused_slot_head;
 #if TEST
 	printf("slot_list init\n");
 #endif
-	for(int i = 0; i < SLOT_NUM; ++i){
-		current = malloc(sizeof(ip_pcb_t));
-		if(current == NULL){
-			perror("init_buffer: ");
-			clean_exit();
-		}
-		memset(current, 0, sizeof(ip_pcb_t));
-		prev->next = current;
-		prev = current;
+			for(int i = 0; i < SLOT_NUM; ++i){
+				current = malloc(sizeof(ip_pcb_t));
+				if(current == NULL){
+					perror("init_buffer: ");
+					clean_exit();
+				}
+				memset(current, 0, sizeof(ip_pcb_t));
+				prev->next = current;
+				prev = current;
+			}
+			break;
+		case(SENDER):
+			full_num = 0;
+			send_buffer_head = malloc(sizeof(ip_pcb_t));
+			memset(send_buffer_head, 0, sizeof(ip_pcb_t));
+			last_send_buffer = send_buffer_head;
+
+			receive_buffer_head = malloc(sizeof(ip_pcb_t));
+			memset(receive_buffer_head, 0, sizeof(ip_pcb_t));
+			last_receive_buffer = receive_buffer_head;
+			break;
+		case(RECEIVER):
+			full_num = 0;
+
+			receive_buffer_head = malloc(sizeof(ip_pcb_t));
+			memset(receive_buffer_head, 0, sizeof(ip_pcb_t));
+			last_receive_buffer = receive_buffer_head;
+			break;
+		default:
+			printf("error rank: %d\n", rank);
+			break;
 	}
 }
 
-int init_switch(int argc, char** argv){
-	rank = SWITCH;
+int init_switch(){
+	// 初始化规则列表
+	parse_rulefile("src/switch.config");
 
-	//初始化规则列表
-	if(argc >= 4)
-		parse_rulefile(argv[3]);
-	else
-		parse_rulefile("src/switch.config");
-
-	//初始化端口
-	port_num = atoi(argv[2]);
+	// 初始化端口
+	port_num = 2;
 	init_port();
 
-	//初始化缓冲区
+	// 初始化包处理函数
+	set_packet_processor();
+
+	// 初始化缓冲区
 	init_buffer();
 
-	//初始化线程
+	// 初始化线程
 	writer_num = port_num;
 
-	//初始化锁
+	// 初始化锁
 	pthread_mutex_init(&packet_num_mutex, NULL);
 	pthread_mutex_init(&slot_mutex, NULL);
-	pthread_cond_init(&empty, NULL);
-	pthread_cond_init(&full, NULL);
+	pthread_cond_init(&slot_empty, NULL);
+	pthread_cond_init(&slot_full, NULL);
 	return 0;
 }
 
 //checksum检查，获得锁然后写入缓冲区（链表操作），释放锁
-void run_writer(unsigned char *argument, const struct pcap_pkthdr *packet_heaher, const unsigned char *packet_content){
-
+void write_buffer(unsigned char *argument, const struct pcap_pkthdr *packet_header, const unsigned char *packet_content){
 #if TEST
 	printf("run_writer\n");
 #endif
-
-	//从端口2发出去的包不能收
-	if(strcmp(argument, "10.0.1.0") == 0){
-		return;
-	}
-
 	pthread_mutex_lock(&packet_num_mutex);
+	receive_packet_num++;
+	
+	time_stamp = clock();
+
 #if TEST
 	printf("receive packet num: %d\n", receive_packet_num);
 #else
-	if(receive_packet_num % 10 == 0)
-		printf("receive_packet_num == %d\n", receive_packet_num);
+	if(receive_packet_num % 50 == 0)
+		printf("receive_packet_num = %d\n", receive_packet_num);
 #endif
 	pthread_mutex_unlock(&packet_num_mutex);
 
 	//checksum检查
-    ip_header_t * ip_head = (ip_header_t *)packet_content;
-
-	char buf[BUFFER_SIZE]={};
-    memcpy(buf, packet_content, ip_head->length);
-    ip_head = (ip_header_t *)buf;
-
-    uint16_t tmp_checksum = ip_head->checksum;
-    ip_head->checksum = 0;
-    if(tmp_checksum != compute_checksum(buf, ip_head->length)){
-        printf("packet %d: checksum not match\n", receive_packet_num);
-		return;
-    }
-
-	ip_head->checksum = tmp_checksum;
-
-	packet_info_t * packet_info = malloc(sizeof(packet_info_t));
-	if(packet_info == NULL){
-		perror("run_swriter: ");
-		clean_exit();
+	if(!check(packet_content)){
+		printf("Packet %d: checksum not match\n", receive_packet_num);
 	}
-	memset(packet_info, 0, sizeof(packet_info_t));
-	pthread_mutex_lock(&packet_num_mutex);
-	packet_info->packet_id = receive_packet_num;
-	pthread_mutex_unlock(&packet_num_mutex);
-
-	decode_incp_ip(packet_info, buf);
-
-#if TEST
-	print_packet_info(packet_info);
-#endif
-
-	free(packet_info);
 
 
 	//消费者操作
 	pthread_mutex_lock(&slot_mutex);
 	while(empty_num == 0){
-		pthread_cond_wait(&empty, &slot_mutex);
+		pthread_cond_wait(&slot_empty, &slot_mutex);
 	}
 
-	//写入缓冲区（链表操作）tbc
+	//写入缓冲区（链表操作）
 	ip_pcb_t * current = unused_slot_head->next;
-	memcpy(current, buf, ip_head->length);
 	unused_slot_head->next = current->next;
-	current->next = used_slot_head->next;
-	used_slot_head->next = current;
-
+	memcpy(current, packet_content, packet_header->len);
+	// 插入used_slot队列最后
+	last_used_slot->next = current;
+	last_used_slot = current;
+	last_used_slot->next = NULL;
 	empty_num--;
-	pthread_cond_signal(&full);
+
+	pthread_cond_signal(&slot_full);
 	pthread_mutex_unlock(&slot_mutex);
 
 	return;
 }
 
-int match_and_send(char * data){
+int match_and_send(ip_pcb_t * ip_pcb){
 #if TEST
 	printf("match_and_send\n");
 #endif
-
-	packet_info_t * packet_info = malloc(sizeof(packet_info_t));
-	if(packet_info == NULL){
-		perror("match_and_send: ");
-		clean_exit();
-	}
-	memset(packet_info, 0, sizeof(packet_info_t));
-
-
-	packet_info->packet_id = send_packet_num;
-	decode_incp_ip(packet_info, data);
+	ip_header_t * ip_head = (ip_header_t *)ip_pcb;
 
 	rule_t * prev_rule = action_list;
 	rule_t *curr_rule = prev_rule->next;
 	while(curr_rule != NULL){
-		if(curr_rule->src_ip == packet_info->src_ip){
-			open_pcap(dev_group[curr_rule->output_port-1], &(pcap_handle_group[port_num]));
-
-
-			int send_bytes = pcap_inject(pcap_handle_group[port_num], data, packet_info->length_of_ip);
-			if(send_bytes != packet_info->length_of_ip){
+		if(curr_rule->src_ip == ip_head->src_ip){
+			int send_bytes = pcap_inject(pcap_handle_group[curr_rule->output_port-1], ip_pcb, ip_head->length);
+			if(send_bytes != ip_head->length){
 				perror("packet damage: ");
         	}
 		send_packet_num++;
-		
+	
 #if TEST
-	printf("send packet %d successesfully\n\n", send_packet_num);
+	incp_header_t * incp_head = (incp_header_t *)(ip_pcb->data);
+	struct in_addr addr;
+	memcpy(&addr, &(ip_head->dst_ip), 4);
+	printf("send packet %d to %s: seq_num = %d\n", send_packet_num, inet_ntoa(addr), incp_head->seq_num);
 #else
-	if(send_packet_num % 10 == 0)
-			printf("send_packet_num = %d\n", send_packet_num);
+	if(send_packet_num % 50 == 0)
+		printf("send_packet_num = %d\n", send_packet_num);
+
 #endif
-			
-			free(packet_info);
-			if(pcap_handle_group[port_num] != NULL)
-				pcap_close(pcap_handle_group[port_num]);
-
-
 			return 1;
 		}
 		prev_rule = curr_rule;
 		curr_rule = prev_rule->next;
 	}
-	free(packet_info);
 	return -1;
 }
 
@@ -307,27 +284,35 @@ void * run_reader(void *arg){
 #endif
 	while(1){
 #if TEST
-	printf("I'm reader\n");
+	printf("\nI'm reader\n");
 #endif		
 		//生产者操作
 		pthread_mutex_lock(&slot_mutex);
 		while(empty_num == SLOT_NUM){
-			pthread_cond_wait(&full, &slot_mutex);
+			pthread_cond_wait(&slot_full, &slot_mutex);
 		}
 
-		//遍历，读出，调整链表tbc
+		//遍历，读出，调整链表
 		ip_pcb_t * current = used_slot_head->next;
 		if(current != NULL)
 		{
 			used_slot_head->next = current->next;
+			if(used_slot_head->next == NULL){
+				last_used_slot = used_slot_head;
+			}
 			pthread_mutex_unlock(&slot_mutex);
-			int res = match_and_send((char*)current);
+			int res = match_and_send(current);
 			if(res < 0){
-				printf("error packet: cannot found port to send\n");
+				printf("error packet: cannot found port to send: ");
+			
+				struct in_addr addr;
+				memcpy(&addr, &(((ip_header_t*)current)->src_ip), sizeof(uint32_t));
+				printf("src_ip = %s\n", inet_ntoa(addr));
 			}
 			memset(current, 0, sizeof(ip_pcb_t));
 			pthread_mutex_lock(&slot_mutex);
 			empty_num++;
+
 			current->next = unused_slot_head->next;
 			unused_slot_head->next = current;
 		}
@@ -336,10 +321,19 @@ void * run_reader(void *arg){
 			printf("slot error: empty_num = %d current == NULL\n", empty_num);
 		}
 
-		pthread_cond_signal(&empty);
+		pthread_cond_signal(&slot_empty);
 		pthread_mutex_unlock(&slot_mutex);
 	}
 	
+}
+
+//switch设置包处理器，开始收包
+void * run_writer(void *arg){
+	int idx = *((int*)arg);
+	if(pcap_loop(pcap_handle_group[idx], -1, grinder, NULL) < 0)
+	{
+		perror("pcap_loop: ");
+	}	
 }
 
 //三种终端的资源回收退出程序
@@ -347,37 +341,90 @@ void clean_exit(){
 	switch (rank)
 	{
 	case RECEIVER:
-		if(pcap_handle != NULL)
-			pcap_close(pcap_handle);
-		if(fp != NULL)
-			fclose(fp);
+		{
+			if(pcap_handle != NULL)
+				pcap_close(pcap_handle);
+			if(fp != NULL)
+				fclose(fp);
 
-		printf("total received packet: %d\n", receive_packet_num);
-		printf("Receiver Exit...\n");
+			for(int i = 0; i < conn_num; ++i){
+				free(recv_states[i].addr);
+			}
 
-		exit(0);
+			//buffer
+			pthread_mutex_destroy(&receive_buffer_mutex);
+			ip_pcb_t* prev = receive_buffer_head;
+			ip_pcb_t* current = prev->next;
+			while(current != NULL){
+				prev = current;
+				current = prev->next;
+				free(prev);
+			}
+			free(receive_buffer_head);
+
+			printf("total received packet: %d\n", receive_packet_num);
+			printf("Receiver Exit...\n\n\n");
+
+			exit(0);
+		}
 		break;
 	case SENDER:
-		if(pcap_handle != NULL)
-			pcap_close(pcap_handle);
-		if(fp != NULL)
-			fclose(fp);
+		{
+			if(pcap_handle != NULL)
+				pcap_close(pcap_handle);
+			if(fp != NULL)
+				fclose(fp);
+			
+			//task_queue
+			task_t * prev_task = task_queue;
+			task_t * curr_task = task_queue->next;
+			while (curr_task != NULL)
+			{
+				prev_task = curr_task;
+				curr_task = prev_task->next;
+				free(prev_task);
+			}
+			free(task_queue);
 
-		printf("total sended packet: %d\n", send_packet_num);
-		printf("Sender Exit...\n");
+			//buffer
+			ip_pcb_t* prev = receive_buffer_head;
+			ip_pcb_t* current = prev->next;
+			while(current != NULL){
+				prev = current;
+				current = prev->next;
+				free(prev);
+			}
+			free(receive_buffer_head);
 
-		exit(0);
+			prev = send_buffer_head;
+			current = prev->next;
+			while(current != NULL){
+				prev = current;
+				current = prev->next;
+				free(prev);
+			}
+			free(send_buffer_head);
+
+			// 锁
+			pthread_mutex_destroy(&fp_mutex);
+			pthread_mutex_destroy(&receive_buffer_mutex);
+			pthread_mutex_destroy(&task_mutex);
+			pthread_cond_destroy(&task_queue_empty);
+			pthread_cond_destroy(&task_queue_full);
+			
+			printf("total sended packet: %d\n", send_packet_num);
+			printf("Sender Exit...\n\n\n");
+
+			exit(0);
+		}
 		break;
 	case SWITCH:
-		for(int i = 0; i <= writer_num; ++i){
+		{for(int i = 0; i <= writer_num; ++i){
 			if(pcap_handle_group[i] != NULL)
 				pcap_close(pcap_handle_group[i]);
 		}
-		
-		if(fp != NULL)
-			fclose(fp);
 
-		//actionlist
+		// actionlist
 		rule_t * prev_rule = action_list;
 		rule_t * curr_rule = prev_rule->next;
 		while(curr_rule != NULL){
@@ -405,36 +452,44 @@ void clean_exit(){
 			current = prev->next;
 		}
 		free(unused_slot_head);
-		
+	
 		//锁
 		pthread_mutex_destroy(&packet_num_mutex);
 		pthread_mutex_destroy(&slot_mutex);
-   		pthread_cond_destroy(&empty);
-		pthread_cond_destroy(&full);
+   		pthread_cond_destroy(&slot_empty);
+		pthread_cond_destroy(&slot_full);
 
-		printf("Switch Exit...\n");
-		exit(0);
+		printf("Switch Exit...\n\n\n");
+		exit(0);}
+		break;
+	case UNBLOCK_SWITCH:
+		{printf("error rank: unblock switch\n");
+		printf("Exit...\n");
+		exit(0);}
 		break;
 	default:
-		printf("error rank: %d\n", rank);
+		{printf("error rank: %d\n", rank);
 		printf("Exit...\n");
-		exit(0);
+		exit(0);}
 		break;
 	}
 }
 
-int run_switch(){
+void run_switch(){
 #if TEST
 	printf("\nrun_switch\n");
 	printf("create writers\n");
 #endif
+	signal(SIGTERM, host_end);
+	signal(SIGALRM, daemon_end);
 	
 	int port_group[writer_num];
 	for(int i = 0; i < writer_num; ++i){
 		port_group[i] = i;
-		int res = pthread_create(&(writer_list[i]), NULL, run_receiver, &(port_group[i]));
+		int res = pthread_create(&(writer_list[i]), NULL, run_writer, &(port_group[i]));
 		if(res < 0){
 			printf("pthread_create error: %d\n", res);
+			clean_exit();
 		}
 	}
 	
@@ -445,22 +500,93 @@ int run_switch(){
 	int res = pthread_create(&reader, NULL, run_reader, NULL);
 	if(res < 0){
 		printf("pthread_create error: %d\n", res);
+		clean_exit();
 	}
 
 
-	for(int i = 0; i < writer_num; ++i)
-	{
+	for(int i = 0; i < writer_num; ++i){
 		pthread_join(writer_list[i], NULL);
 #if TEST
 		printf("pthread_join: writer %d\n", i);
 #endif
 	}
+	pthread_join(reader, NULL);
+#if TEST	
+	printf("pthread_join: reader\n");
+#endif	
+	clean_exit();
+	
+	return;
+}
 
+//使用dispatch循环接收包的switch
+void run_unblock_switch(){
+#if TEST
+	printf("\nrun_unblock_switch\n");
+	printf("create writer\n");
+#endif
+
+	int res = pthread_create(&writer, NULL, run_unblock_receiver, NULL);
+	if(res < 0){
+			printf("pthread_create error: %d\n", res);
+			clean_exit();
+	}
+#if TEST
+	printf("create reader\n");
+#endif
+
+	res = pthread_create(&reader, NULL, run_reader, NULL);
+	if(res < 0){
+		printf("pthread_create error: %d\n", res);
+		clean_exit();
+	}
+
+
+	pthread_join(writer, NULL);
+#if TEST	
+	printf("pthread_join: writer\n");
+#endif
+
+
+	pthread_join(reader, NULL);
 #if TEST	
 	printf("pthread_join: reader\n");
 #endif
-
-	pthread_join(reader, NULL);
-	return 0;
+	return;
 }
 
+//一个线程循环接收各个端口上的包
+void * run_unblock_receiver(){
+	//循环逻辑，调用run_writer
+#if TEST
+    printf("run_unblock_receiver\n");
+#endif
+	set_packet_processor();
+	char errbuf[BUFFER_SIZE];
+
+#if TEST
+    printf("open_pcap\n");
+#endif
+	//打开所有端口
+	for(int i = 0; i < port_num; ++i){
+		open_pcap(dev_group[i], &(pcap_handle_group[i]));
+		pcap_setnonblock(pcap_handle_group[i], 1, errbuf);
+	}
+
+	//循环接收包
+	while(1){
+		for(int i = 0; i < port_num; ++i){
+			if(i == 1)//略过端口2
+				continue;
+			if(pcap_dispatch(pcap_handle_group[i], -1, grinder, NULL) < 0)
+			{
+				perror("pcap_dispatc: ");
+			}
+		}
+	}
+
+	//关闭
+	for(int i = 0; i < port_num; ++i){
+		pcap_close(pcap_handle_group[i]);
+	}
+}
